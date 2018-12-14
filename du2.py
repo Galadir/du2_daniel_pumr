3e767a78b5116d4c7e070e42957cc96f4bbba993
Galadir <danpumr@gmail.com>
2018-12-14 22:29:20 +0100

    zjistovani souradnic
    Vytvoreni cyklu, ktery v geojson z features - geometry vytiskne souradnice.

--- du2.py
+++ du2.py
@@ -2,3 +2,16 @@ import json
 
 with open("drinking_water.geojson",encoding="utf-8") as f:
     soubor = json.load(f)
+
+#print(type(soubor)) #zjistím o jaký se jedný typ → dic= slovník
+#for k in soubor: #chci zjistit jaké jsou klíče
+   #print(k)
+
+
+features = soubor['features']
+#print(type(soubor['features']))
+for i in features:
+    geo = i["geometry"]
+    coord = geo["coordinates"]
+    print(coord)
+
