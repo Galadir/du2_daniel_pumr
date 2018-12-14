c93a71bd3f7e9049446cfe188f6338be25377cae
Galadir <danpumr@gmail.com>
2018-12-15 00:19:55 +0100

    Stanovení okraju
    vypis bodů na krajních pozicich pro tvorbu bounding boxu

--- du2.py
+++ du2.py
@@ -7,11 +7,27 @@ with open("drinking_water.geojson",encoding="utf-8") as f:
 #for k in soubor: #chci zjistit jaké jsou klíče
    #print(k)
 
+souradnice=[]
 
 features = soubor['features']
 #print(type(soubor['features']))
 for i in features:
     geo = i["geometry"]
     coord = geo["coordinates"]
-    print(coord)
+    #print(coord)
+    souradnice.append(coord)
 
+print(souradnice)
+
+dataX = sorted(souradnice, key=lambda x: x[0])
+left_edge = dataX[0]
+right_edge = dataX[-1]
+
+dataY = sorted(souradnice, key=lambda y: y[1])
+bottom_edge = dataY[0]
+top_edge = dataY[-1]
+
+print(left_edge)
+print(right_edge)
+print(bottom_edge)
+print(top_edge)
