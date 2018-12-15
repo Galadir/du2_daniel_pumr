cd4d5bb4e5fe0ac780de1eaad70dfd06c7e32d13
Galadir <danpumr@gmail.com>
2018-12-15 16:54:05 +0100

    hotova kostra
    dokonceni zakladniho skriptu bez komentaru, docstringu, testu, chyb...

--- du2.py
+++ du2.py
@@ -56,7 +56,7 @@ def sector_edges(sector,le,re,be,te):
     return S_le,S_re,S_be,S_te
 
 
-def sectors(soubor,le,re,be,te):
+def sectors(soubor,sec,le,re,be,te):
     """
     Funkce přiřadí bodům do properties atribut cluster_id podle too v jakém sektoru se nachází
     :param soubor:nahraná data z geoJSON
@@ -71,58 +71,48 @@ def sectors(soubor,le,re,be,te):
     cy = (be+te)/2
 
 
-    for i in soubor['features']:
-        if i["geometry"]["coordinates"][0] < cx and i["geometry"]["coordinates"][1] > cy:
-            i["properties"]["cluster_id"] += 'A'
+    for i in sec:
+        if soubor['features'][i]["geometry"]["coordinates"][0] < cx and soubor['features'][i]["geometry"]["coordinates"][1] > cy:
+            soubor['features'][i]["properties"]["cluster_id"] += 'A'
             sectorA.append(i)
-        elif i["geometry"]["coordinates"][0] > cx and i["geometry"]["coordinates"][1] > cy:
-            i["properties"]["cluster_id"] += 'B'
+        elif soubor['features'][i]["geometry"]["coordinates"][0] > cx and soubor['features'][i]["geometry"]["coordinates"][1] > cy:
+            soubor['features'][i]["properties"]["cluster_id"] += 'B'
             sectorB.append(i)
-        elif i["geometry"]["coordinates"][0] < cx and i["geometry"]["coordinates"][1] < cy:
-            i["properties"]["cluster_id"] += 'C'
+        elif soubor['features'][i]["geometry"]["coordinates"][0] < cx and soubor['features'][i]["geometry"]["coordinates"][1] < cy:
+            soubor['features'][i]["properties"]["cluster_id"] += 'C'
             sectorC.append(i)
-        elif i["geometry"]["coordinates"][0] > cx and i["geometry"]["coordinates"][1] < cy:
-            i["properties"]["cluster_id"] += 'D'
+        elif soubor['features'][i]["geometry"]["coordinates"][0] > cx and soubor['features'][i]["geometry"]["coordinates"][1] < cy:
+            soubor['features'][i]["properties"]["cluster_id"] += 'D'
             sectorD.append(i)
 
-    print(sectorA)
-    print(sectorB)
-    print(sectorC)
-    print(sectorD)
+    sectorA = tuple(sectorA)
+    sectorB = tuple(sectorB)
+    sectorC = tuple(sectorC)
+    sectorD = tuple(sectorD)
 
-    if len(sectorA)>10:
+    if len(sectorA)>50:
         S_e=sector_edges('A',le,re,be,te)
-        (soubor,sectorA, S_e[0], S_e[1], S_e[2], S_e[3])
-    if len(sectorB)>10:
+        sectors(soubor,sectorA, S_e[0], S_e[1], S_e[2], S_e[3])
+    if len(sectorB)>50:
         S_e=sector_edges('B',le,re,be,te)
-        (soubor,sectorB, S_e[0], S_e[1], S_e[2], S_e[3])
-    if len(sectorC)>10:
+        sectors(soubor,sectorB, S_e[0], S_e[1], S_e[2], S_e[3])
+    if len(sectorC)>50:
         S_e=sector_edges('C',le,re,be,te)
-        (soubor,sectorC, S_e[0], S_e[1], S_e[2], S_e[3])
-    if len(sectorD)>10:
+        sectors(soubor,sectorC, S_e[0], S_e[1], S_e[2], S_e[3])
+    if len(sectorD)>50:
         S_e=sector_edges('D',le,re,be,te)
-        (soubor,sectorD,S_e[0], S_e[1], S_e[2], S_e[3])
+        sectors(soubor,sectorD,S_e[0], S_e[1], S_e[2], S_e[3])
 
 
 for i in soubor['features']:
     i["properties"]["cluster_id"] = ''
 
-# sector_calc=[]
-# for i in soubor['features']:
-#     sector_calc.append(i)
-#     print(sector_calc)
-
 sec= range(len(soubor['features']))
 print(sec)
 
 e=edges(soubor)
-sectors(soubor,e[0],e[1],e[2],e[3])
+sectors(soubor,sec,e[0],e[1],e[2],e[3])
 
 
-features = soubor['features'][1]
-print(features)
-
-
-
-with open('drinking_water_pokus6.geojson', mode = 'w',encoding='UTF-8') as f:
+with open('drinking_water_pokus10.geojson', mode = 'w',encoding='UTF-8') as f:
     json.dump(soubor,f)
