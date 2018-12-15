2ae5472ec8d80027f7426c21b90e59ba6b51583e
Galadir <danpumr@gmail.com>
2018-12-15 11:18:35 +0100

    vytvoreni funkce edges
    zabalení kodu na tvorbu krajnich bodu bboxu do funkce

--- du2.py
+++ du2.py
@@ -3,31 +3,39 @@ import json
 with open("drinking_water.geojson",encoding="utf-8") as f:
     soubor = json.load(f)
 
-#print(type(soubor)) #zjistím o jaký se jedný typ → dic= slovník
-#for k in soubor: #chci zjistit jaké jsou klíče
-   #print(k)
-
 souradnice=[]
-
 features = soubor['features']
-#print(type(soubor['features']))
 for i in features:
     geo = i["geometry"]
     coord = geo["coordinates"]
-    #print(coord)
     souradnice.append(coord)
 
 print(souradnice)
 
-dataX = sorted(souradnice, key=lambda x: x[0])
-left_edge = dataX[0]
-right_edge = dataX[-1]
+def edges(souradnice):
+    """
+    Funkce vytvoří ze souřadnic okraje bboxu
+    :param souradnice: list souřadnic
+    :return: množina krajních bodů (left,right, bottom, top, centerX,centerY)
+    """
+    dataX = sorted(souradnice, key=lambda x: x[0])
+    left = dataX[0]
+    right = dataX[-1]
 
-dataY = sorted(souradnice, key=lambda y: y[1])
-bottom_edge = dataY[0]
-top_edge = dataY[-1]
+    dataY = sorted(souradnice, key=lambda y: y[1])
+    bottom = dataY[0]
+    top = dataY[-1]
 
-print(left_edge)
-print(right_edge)
-print(bottom_edge)
-print(top_edge)
+    left_edge = left[0]
+    right_edge = right[0]
+    bottom_edge = bottom[1]
+    top_edge = top[1]
+    center_X = (left_edge+right_edge)/2
+    center_Y = (bottom_edge+top_edge)/2
+
+    return(left_edge,right_edge,bottom_edge,top_edge,center_X,center_Y)
+
+ans=edges(souradnice)
+print(ans)
+
+
