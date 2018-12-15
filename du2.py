8db0ef9f0f0fd626ec8b7e1aa64f5536f929abcc
Galadir <danpumr@gmail.com>
2018-12-15 16:22:47 +0100

    ztraceni uprostred funkce
    práce na rekurzi

--- du2.py
+++ du2.py
@@ -3,26 +3,19 @@ import json
 with open("drinking_water.geojson",encoding="utf-8") as f:
     soubor = json.load(f)
 
-# print(type(soubor)) #zjistím o jaký se jedný typ → dic= slovník
-# for k in soubor: #chci zjistit jaké jsou klíče
-#    print(k)
 
-souradnice=[]
-features = soubor['features']
-print(features)
-for i in features:
-    geo = i["geometry"]
-    coord = geo["coordinates"]
-    souradnice.append(coord)
-
-print(souradnice)
-
-def edges(souradnice):
+def edges(soubor):
     """
     Funkce vytvoří ze souřadnic okraje bboxu
-    :param souradnice: list souřadnic
+    :param soubor: nahraná data z geoJSON
     :return: množina krajních bodů (left,right, bottom, top, centerX,centerY)
     """
+
+    souradnice = []
+    for i in soubor['features']:
+        coord = i["geometry"]["coordinates"]
+        souradnice.append(coord)
+
     dataX = sorted(souradnice, key=lambda x: x[0])
     left = dataX[0]
     right = dataX[-1]
@@ -35,20 +28,93 @@ def edges(souradnice):
     right_edge = right[0]
     bottom_edge = bottom[1]
     top_edge = top[1]
-    center_X = (left_edge+right_edge)/2
-    center_Y = (bottom_edge+top_edge)/2
 
-    return(left_edge,right_edge,bottom_edge,top_edge,center_X,center_Y)
 
-# ans=edges(souradnice)
-# print(ans)
+    return left_edge,right_edge,bottom_edge,top_edge
+
+def sector_edges(sector,le,re,be,te):
+    if sector == 'A':
+        S_le=le
+        S_re=(le+re)/2
+        S_be=(be+te)/2
+        S_te=te
+    elif sector == 'B':
+        S_le=(le+re)/2
+        S_re=re
+        S_be=(be+te)/2
+        S_te=te
+    elif sector == 'C':
+        S_le=le
+        S_re=(le+re)/2
+        S_be=be
+        S_te=(be+te)/2
+    elif sector == 'D':
+        S_le=(le+re)/2
+        S_re=re
+        S_be=be
+        S_te=(be+te)/2
+    return S_le,S_re,S_be,S_te
+
+
+def sectors(soubor,sec,le,re,be,te):
+    """
+    Funkce přiřadí bodům do properties atribut cluster_id podle too v jakém sektoru se nachází
+    :param soubor:nahraná data z geoJSON
+    :return:
+    """
+    sectorA=[]
+    sectorB=[]
+    sectorC=[]
+    sectorD=[]
+
+    cx = (le+re)/2
+    cy = (be+te)/2
+
+
+    for i in range(len(soubor['features'])):
+        if i["geometry"]["coordinates"][0] < cx and i["geometry"]["coordinates"][1] > cy:
+            i["properties"]["cluster_id"] += 'A'
+            sectorA.append(i)
+        elif i["geometry"]["coordinates"][0] > cx and i["geometry"]["coordinates"][1] > cy:
+            i["properties"]["cluster_id"] += 'B'
+            sectorB.append(i)
+        elif i["geometry"]["coordinates"][0] < cx and i["geometry"]["coordinates"][1] < cy:
+            i["properties"]["cluster_id"] += 'C'
+            sectorC.append(i)
+        elif i["geometry"]["coordinates"][0] > cx and i["geometry"]["coordinates"][1] < cy:
+            i["properties"]["cluster_id"] += 'D'
+            sectorD.append(i)
+
+    if len(sectorA)>10:
+        S_e=sector_edges('A',le,re,be,te)
+        (soubor,sectorA, S_e[0], S_e[1], S_e[2], S_e[3])
+    if len(sectorB)>10:
+        S_e=sector_edges('B',le,re,be,te)
+        (soubor,sectorB, S_e[0], S_e[1], S_e[2], S_e[3])
+    if len(sectorC)>10:
+        S_e=sector_edges('C',le,re,be,te)
+        (soubor,sectorC, S_e[0], S_e[1], S_e[2], S_e[3])
+    if len(sectorD)>10:
+        S_e=sector_edges('D',le,re,be,te)
+        (soubor,sectorD,S_e[0], S_e[1], S_e[2], S_e[3])
+
 
 for i in soubor['features']:
-    i["properties"]["cluster_id"] = 1
+    i["properties"]["cluster_id"] = ''
+
+sector_calc=[]
+for i in soubor['features']:
+    sector_calc.append(i)
+    print(sector_calc)
+
+e=edges(soubor)
+sectors(soubor,sector_calc,e[0],e[1],e[2],e[3])
+
 
 features = soubor['features']
 print(features)
 
 
-with open('drinking_water_pokus.geojson', mode = 'w',encoding='UTF-8') as f:
+
+with open('drinking_water_pokus6.geojson', mode = 'w',encoding='UTF-8') as f:
     json.dump(soubor,f)
